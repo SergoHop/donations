08031177d433fb0b35e81daa072c4933781301a2
Сергей Хопин <sergejhopin@Air-Sergej.Dlink>
2025-03-27 17:33:38 +0300

    пишется servaices, email,id + некие изменения

--- internal/repositories/interfaces.go
+++ internal/repositories/interfaces.go
@@ -1,11 +1,14 @@
 package repositories
 
-import "mydonate/internal/models"
+import (
+    "context"
+    "mydonate/internal/models"
+)
 
-type UserRepository interface {
-    Create(user *models.User) error
-    GetByEmail(email string) (*models.User, error)
-    GetByID(id uint) (*models.User, error)
-    UpdateVerificationCode(email string, code string) error
-	MarkVerified(email string) error
+type UserService interface {
+	Create(ctx context.Context, user *models.User) error
+	GetByEmail(ctx context.Context, email string) (*models.User, error)
+	GetByID(ctx context.Context, id uint) (*models.User, error)
+	UpdateVerificationCode(ctx context.Context, email string, code string) error
+	MarkVerified(ctx context.Context, email string) error
 }
--- internal/services/services_auth.go
+++ internal/services/services_auth.go
@@ -3,18 +3,18 @@ package services
 import (
     "context"
 	"fmt"
-	"log"
+	_ "log"
 
 	"mydonate/internal/models"       
 	"mydonate/internal/repositories"
 )
 
 type userService struct {
-	userRepo repositories.UserRepository
+	userRepo repositories.UserService
 }
 
 
-func NewUserService(userRepo repositories.UserRepository) UserService {
+func NewUserService(userRepo repositories.UserService) UserService {
 	return &userService{
 		userRepo: userRepo,
 	}
@@ -28,9 +28,32 @@ func (s *userService) Create(ctx context.Context, user *models.User) error {
 		return fmt.Errorf("шо то не то")
 	}
 
-    err := s.userRepo.Create(user)
+    err := s.userRepo.Create(ctx, user)
     if err != nil{
         return fmt.Errorf("фаил ошиька: %w", err)
     }
     return nil
 }
+
+func (s *userService) GetByEmail(ctx context.Context, email string) (*models.User, error) {
+    if email == ""{
+        return nil, fmt.Errorf("шо то не то")
+    }
+    user, err := s.userRepo.GetByEmail(ctx, email)
+    if err != nil{
+        return nil, fmt.Errorf("нема такого имаила", err)
+    }
+    return user, nil
+}
+
+func (s *userService) GetByID(ctx context.Context, id uint) (*models.User, error) {
+    if id == 0{
+        return nil, fmt.Errorf("гдэ айди")
+    }
+    user, err := s.userRepo.GetByID(ctx, id)
+    if err != nil{
+        return nil, fmt.Errorf("а шо с айди", err)
+    }
+    return user, nil
+}
+
